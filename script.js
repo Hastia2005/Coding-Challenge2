e03772be0f9548c8cb8a3d7933e72d08f25513fe
Hastia <ahmadi.dev05@gmail.com>
2025-04-07 11:55:07 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const scoreDolphins=97+112+101;
+const scoreKoalas=109+95+106;
+console.log(scoreDolphins/3,scoreKoalas/3);
+if(scoreDolphins>=100){
+    console.log('dolphins team is win');
+}if(scoreKoalas>=100){
+    console.log('koalas team is win');
+}
+
+
